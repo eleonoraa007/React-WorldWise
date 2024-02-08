b7259ad20af9f6df748b1b9249377ce689a17bf1
eleonoraa007 <hrubenja.eleonora@gmail.com>
2024-02-08 11:04:28 +0100

    first commit

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,28 @@
+import Spinner from "./Spinner";
+import styles from './CountryList.module.css'
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountryList({cities, isLoading}) {
+    if(isLoading) return <Spinner />;
+
+    if(!cities.length) return <Message message="Add your first city by clicking on a city on the map"/>
+
+    //(acc)accumulator will be an arr 
+    //and (curr)current value will be a city
+    const countries = cities.reduce((arr, city) => 
+    // if the current country is not in the array then:
+    {if(!arr.map(el => el.country).includes(city.country))
+    //return a new array which has all the current el + a new one
+    return [...arr, {country: city.country, emoji: city.emoji }];
+    else return arr}
+    , []);
+
+    return (
+        <ul className={styles.countryList}>
+            {countries.map((country) => <CountryItem country={country} key={country.id} />)}
+        </ul>
+    )
+}
+
+export default CountryList
